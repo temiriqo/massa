13e152670e395fde60ab51bb797e159eaa33f8eb
AurelienFT <aurelien.foucault@epitech.eu>
2022-03-30 16:56:44 +0200

    Add flurry.

--- massa-storage/src/lib.rs
+++ massa-storage/src/lib.rs
@@ -1,7 +1,7 @@
-use massa_models::prehash::Map;
+use flurry::HashMap;
+use massa_models::prehash::BuildMap;
 use massa_models::{Block, BlockId};
 use parking_lot::RwLock;
-use std::collections::hash_map::Entry;
 use std::sync::Arc;
 
 #[derive(Debug)]
@@ -18,36 +18,33 @@ pub struct StoredBlock {
 /// A storage of block, shared by various components.
 #[derive(Clone, Default)]
 pub struct Storage {
-    blocks: Arc<RwLock<Map<BlockId, Arc<RwLock<StoredBlock>>>>>,
+    blocks: Arc<HashMap<BlockId, Arc<RwLock<StoredBlock>>, BuildMap<BlockId>>>,
 }
 
 impl Storage {
     /// Store a block, along with it's serialized representation.
     pub fn store_block(&self, block_id: BlockId, block: Block, serialized: Vec<u8>) {
-        let mut blocks = self.blocks.write();
-        match blocks.entry(block_id) {
-            Entry::Occupied(_) => {}
-            Entry::Vacant(entry) => {
-                let stored_block = StoredBlock {
-                    block,
-                    serialized,
-                    serialized_header: None,
-                };
-                let to_store = Arc::new(RwLock::new(stored_block));
-                entry.insert(to_store);
-            }
+        let blocks = self.blocks.pin();
+        if blocks.contains_key(&block_id) {
+            let stored_block = StoredBlock {
+                block,
+                serialized,
+                serialized_header: None,
+            };
+            let to_store = Arc::new(RwLock::new(stored_block));
+            blocks.insert(block_id, to_store);
         }
     }
 
     /// Get a (mutable) reference to the stored block.
     pub fn retrieve_block(&self, block_id: &BlockId) -> Option<Arc<RwLock<StoredBlock>>> {
-        let blocks = self.blocks.read();
+        let blocks = self.blocks.pin();
         blocks.get(block_id).map(Arc::clone)
     }
 
     /// Remove a list of blocks from storage.
     pub fn remove_blocks(&self, block_ids: &[BlockId]) {
-        let mut blocks = self.blocks.write();
+        let blocks = self.blocks.pin();
         for id in block_ids {
             blocks.remove(id);
         }
